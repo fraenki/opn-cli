7cfaaa92e7dfa977c6c15d3e6cd85aa4f634129d
Frank Wall <fw@moov.de>
2023-02-14 13:42:27 +0100

    WIP

---
+++ opnsense_cli/api/plugin/apibackup.py
@@ -0,0 +1,13 @@
+from opnsense_cli.api.base import ApiBase
+
+class Apibackup(ApiBase):
+    MODULE = "apibackup"
+    CONTROLLER = "backup"
+    """
+    api-backup BackupController
+    """
+
+    @ApiBase._api_call
+    def download(self, *args, json=None):
+        self.method = "get"
+        self.command = "download"
